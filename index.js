aaf7251b9e4fe2ec80fe90169a15fad0c30e5e3d
Aman Bansal <amanbansal@github.com>
2018-05-28 08:38:31 +0530

    cdn materialize used directly run the html page

---
+++ index.js
@@ -0,0 +1,27 @@
+//Get Input element
+let filterInput = document.getElementById('filterInput');
+//add Event Listener
+filterInput.addEventListener('keyup',filterNames);
+
+function filterNames(){
+    //get value of input
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    console.log(filterValue);
+
+    //get names ul
+    let ul = document.getElementById('names');
+
+    //get items
+    let li = ul.querySelectorAll('li.collection-item');
+
+    //looop through collection item lis
+    for(let i =0 ; i< li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        //if matched
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            li[i].style.display = '';
+        }else{
+            li[i].style.display = 'none';
+        }
+    }
+}
